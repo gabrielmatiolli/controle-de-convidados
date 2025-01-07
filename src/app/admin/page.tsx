75347438461022eb6b34ee7dabef3a65c436f919
Gabriel Matiolli <gabrielmatiollif@gmail.com>
2025-01-07 16:05:37 -0300

    Criação da tela de admin

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,8 +1,36 @@
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
+import { sql } from '@vercel/postgres'
 import React from 'react'
 
-function Page() {
+async function Page() {
+
+  const { rows } = await sql`SELECT * FROM convidados`
+
   return (
-    <div>Page</div>
+    <main className='bg-amber-100 w-full min-h-screen flex items-center justify-start py-20 flex-col gap-4'>
+      <h1 className='text-2xl font-montserrat'>Lista de convidados</h1>
+
+      <Table>
+        <TableCaption>Lista de convidados.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Nome</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Ação</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.id}>
+              <TableCell className="font-medium">{row.nome}</TableCell>
+              <TableCell>{row.situacao}</TableCell>
+              <TableCell className="text-right">$250.00</TableCell>
+            </TableRow>
+          ))}
+
+        </TableBody>
+      </Table>
+    </main>
   )
 }
 
